25a33fa07f976437c6d637af33b50e6e4b3a019d
Paolo <paooolino@gmail.com>
2019-05-14 15:11:26 +0200

    bootstrap

--- BGame/templates/default/partials/publicnav.php
+++ BGame/templates/default/partials/publicnav.php
@@ -1,5 +1,10 @@
-<ul>
-  <?php foreach ($leagueslist as $league) { ?>
-  <li><a href="<?php echo $router->pathFor("LEAGUE", ["url" => $league["url"]]); ?>"><?php echo $league["name"]; ?></a></li>
+<ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" href="<?php echo $router->pathFor("HOME"); ?>">Home</a>
+  </li>
+  <?php foreach ($leagues as $league) { ?>
+  <li class="nav-item">
+    <a class="nav-link" href="<?php echo $router->pathFor("LEAGUE", ["id" => $league["id"]]); ?>"><?php echo $league["name"]; ?></a>
+  </li>
   <?php } ?>
 </ul>
